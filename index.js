51ee2d4d798228b5621eac1a78b0b845d97a1a91
ferretdayo <ferretdayo@hotmail.co.jp>
2018-05-20 23:04:03 +0900

    a

---
+++ index.js
@@ -0,0 +1,49 @@
+const Botkit = require('botkit')
+const os = require('os')
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment')
+  process.exit(1)
+}
+
+const controller = Botkit.slackbot({
+  debug: true,
+})
+
+const bot = controller.spawn({
+  token: process.env.token
+}).startRTM()
+
+controller.hears(['(.*)って呼んで'], 'direct_message,direct_mention,mention', function (bot, message) {
+
+  // 「◯◯って呼んで」の、◯◯の部分を取り出します。
+  // message.match[1] には、hearsの正規表現にマッチした単語が入っています。
+
+  const name_from_msg = message.match[1]
+
+  // まず、controller.storage.users.getで、ユーザーデータを取得します。
+
+  // message.userには、ユーザーIDが入っています。
+  // ユーザーデータは、ユーザーIDと紐付けていますので、第一引数には、必ずmessage.userを入れます。
+
+  controller.storage.users.get(message.user, function (err, user_info) {
+      // ▼ データ取得後の処理 ▼
+
+      // ユーザーデータが存在しているかどうか調べる
+      // ※第二引数で指定した変数(ここでは'user_info')に、ユーザーデータが入っています。
+      if (!user_info) {
+          // ▼ ユーザーデータがなかった場合の処理 ▼
+          // ユーザーidとユーザー名 のオブジェクトを、user_infoとして作成します。
+          user_info = {
+              id: message.user,
+              name: name_from_msg
+          }
+      }
+
+      // user_infoを保存します。
+      controller.storage.users.save(user_info, function (err, id) {
+          // ▼ 保存完了後の処理▼
+          bot.reply(message, 'あなたのお名前は *' + user_info.name + '* さんですね！覚えました！')
+      })
+  })
+})
