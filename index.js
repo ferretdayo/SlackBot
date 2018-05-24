a2cd48bd7f45618e61e08639dfc8318923ffc28f
ferretdayo <ferretdayo@hotmail.co.jp>
2018-05-24 14:00:04 +0900

    show

--- index.js
+++ index.js
@@ -106,10 +106,11 @@ controller.hears(['(.*)お店(.*)', '(.*)居酒屋(.*)', '(.*)ランチ(.*)', '(
         format: 'json'
       }
     }, (err, response, body) => {
+      console.log(JSON.stringify(body))
       let shops = response.body.results.shop
       shops.forEach(shop => {
         convo.say(shop.name + ", " + shop.urls.pc)
-      });
+      })
       console.log(JSON.stringify(response))
       convo.next()
     })
